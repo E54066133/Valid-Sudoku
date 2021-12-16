5c27acaa7452fd163fdb613b4b1545f5b0c3dadb
HSIAO-HUA HSU <45085286+E54066133@users.noreply.github.com>
2021-12-16 11:31:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        ## brute force
-        for i in range(9):
+        for i in range(9):                                                          # 產生九宮格
             for j in range(9):
                 if board[i][j] == ".":
                     continue
                 else:
-                    # check column
-                    for i_index in range(9):
+                    for i_index in range(9):                                        # 檢查直欄
                         if i_index != i and board[i_index][j] == board[i][j]:
                             return False
                     
-                    #check row
-                    for j_index in range(9):
+                    for j_index in range(9):                                        # 檢查橫列
                         if j_index != j and board[i][j_index] == board[i][j]:
                             return False
                     
-                    # check square
+                    # check square                                                  # 檢查九宮格
                     for i_index in range(3):
                         for j_index in range(3):
                             if (i_index + int(i / 3) * 3) != i and (j_index + int(j / 3) * 3) != j and board[i][j] == board[i_index + int(i / 3) * 3][j_index + int(j / 3) * 3]:
